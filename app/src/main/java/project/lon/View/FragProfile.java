1a3456b291b73d15262ef2adae53350e6079215b
LDK3012 <duykhanglieng@gmail.com>
2024-07-09 12:50:44 +0700

    logout added

--- app/src/main/java/project/lon/View/FragProfile.java
+++ app/src/main/java/project/lon/View/FragProfile.java
@@ -1,10 +1,14 @@
 package project.lon.View;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
@@ -13,8 +17,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 import project.lon.R;
 import project.lon.View.EditProfile;
@@ -25,7 +31,9 @@ public class FragProfile extends Fragment {
     ActionBar actionBar;
     BottomNavigationView btnNav;
     ListView lstView;
-    Button btnEditProfile;
+    Button btnEditProfile, btnLogout;
+
+    FirebaseAuth firebaseAuth;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,6 +49,8 @@ public class FragProfile extends Fragment {
         btnNav = view.findViewById(R.id.btnNav);
         lstView = view.findViewById(R.id.lstActivity);
         btnEditProfile = view.findViewById(R.id.btnEdtProfile);
+        btnLogout = view.findViewById(R.id.btnLogout);
+        firebaseAuth = FirebaseAuth.getInstance();
         AppCompatActivity activity = (AppCompatActivity) getActivity();
         if (activity != null) {
             actionBar = activity.getSupportActionBar();
@@ -59,6 +69,43 @@ public class FragProfile extends Fragment {
                 // Tạo Intent để bắt đầu EditProfile activity
                 Intent intent = new Intent(getActivity(), EditProfile.class);
                 startActivity(intent);
+            }
+        });
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //remove session
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("Confirm");
+                builder.setMessage("Bạn có muốn đăng xuất ?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Remove session
+                        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("status_login", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("name", "false");
+                        editor.apply();
+                        //
+                        firebaseAuth.signOut();
+                        // Start LoginActivity
+                        Intent intent = new Intent(getActivity(), MainActivity.class);
+                        startActivity(intent);
+                        Toast.makeText(getActivity(), "Đăng xuất thành công", Toast.LENGTH_SHORT).show();
+                        getActivity().finish();
+                    }
+                });
+                //
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Dismiss the dialog
+                        dialog.dismiss();
+                    }
+                });
+                //
+                AlertDialog dialog = builder.create();
+                dialog.show();
             }
         });
     }
